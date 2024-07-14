ba9e01a069128825475332c690b3dfa63a677e84
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-14 16:16:16 +0530

    Star pattern

---
+++ 32.cpp
@@ -0,0 +1,19 @@
+// star pattern
+// *
+// **
+// ***
+// ****
+// *****
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+            cout<<"*";
+        cout<<"\n";
+    }
+}

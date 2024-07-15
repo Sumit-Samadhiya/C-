af87f5d1b6b738ad8188d089f23055326cb16a06
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-15 17:42:20 +0530

    // Calculator

--- 41.cpp
+++ 41.cpp
@@ -1,35 +1,41 @@
+// Calculator
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
     int e;
-    cout<<"Enter day name : ";
+    
+    cout<<"Enter Expression: ";
     cin>>e;
 
     int a ,b, c;
+    cout<<"Enter Two number :";
+    cin>>a>>b;
+    
     switch (e)
     {
     case 1:
         c=a+b;
-        cout<<c;
+        cout<<"ADDition = "<<c;
         break;
     
     case 2:
         c=a-b;
-        cout<<"
-          " <<c;
+        cout<<"Subtraction = " <<c;
         break;
+
     case 3:
         c=a*b;
-        cout<<"Multiple "<<c;
+        cout<<"Multiple = "<<c;
         break;
 
     case 4:
         c=a/b;
         cout<<"division = "<<c;
-
         break;
+
     default:
         break;
     }

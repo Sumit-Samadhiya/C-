db00d597c4b8e7388da68ee65445863d1a5084e5
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-17 11:32:15 +0530

    write function to calculate the number of combinations one make from n items and r selected at a time

---
+++ 49.cpp
@@ -0,0 +1,28 @@
+//write function to calculate the number of combinations one make from n items and r selected at a time ;
+
+#include <iostream>
+using namespace std;
+
+
+
+long fact(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; ++i) {
+        fact *= i;
+    }
+    return fact;
+}
+
+
+long combination(int n,int r)
+{
+    return fact(n) / (fact(r) * fact(n-r));
+}
+
+int main() {
+    int n ,r;
+    cout<<"Enter n and r : ";
+    cin>>n>>r;
+
+    cout<<combination(n,r);
+}

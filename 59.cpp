199a0394a193d8db036646d88f580d91a931b384
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-22 12:44:08 +0530

    write a funtion for Print N Natural Number by Recursion;

---
+++ 59.cpp
@@ -0,0 +1,26 @@
+// write a funtion for Print N Natural Number by Recursion;
+
+#include<iostream>
+using namespace std;
+
+int Natural(int n)
+{
+    if(n<=0)
+        return 0;
+    else
+        {
+            Natural(n-1);
+            cout<<n;
+        }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a Number";
+    cin>>n;
+
+    Natural(n);
+
+
+}

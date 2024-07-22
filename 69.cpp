443585c7af5b78ae21241ada97331dc5488bb8e5
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-22 13:37:05 +0530

    write a funtion to Calculate N term of Fibonacci Series by Recursion;

---
+++ 69.cpp
@@ -0,0 +1,34 @@
+// write a funtion to Calculate N term of Fibonacci Series by Recursion;
+
+#include<iostream>
+using namespace std;
+
+int Function(int n, int a,int b)
+{
+    
+    if(n<=0)
+        return 0; 
+    else 
+        {
+           
+           int c=a+b;
+           a=b;
+           b=c;
+           cout<<b;
+           Function(n-1,a,b);
+           
+        }  
+        
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a Number";
+    cin>>n;
+    int a=-1;
+    int b=1;
+    Function(n,a,b);
+    
+}

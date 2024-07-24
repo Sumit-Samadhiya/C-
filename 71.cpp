a8451fa6c63712bbc40404137c8635e1f4417c74
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-24 15:16:48 +0530

    Write a Program to Average of Numbers Stored in Array of size 10

---
+++ 71.cpp
@@ -0,0 +1,23 @@
+// Write a Program to Average of Numbers Stored in Array of size 10 .
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[10];
+    int sum=0;
+    cout<<"Enter the Numbers";
+    for(int i=0;i<10;i++)
+    {
+        cin>>a[i];
+    }
+
+    
+    for(int i=0;i<10;i++)
+    {
+        sum=sum+a[i];
+    }
+    cout<<"The Average is "<<sum/10;
+    
+}

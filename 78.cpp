e85457da30e514c3e070939a8504913283ac9401
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-25 12:31:20 +0530

    // Write a Program to sort Array of size 10 Desending order .

---
+++ 78.cpp
@@ -0,0 +1,37 @@
+// Write a Program to sort Array of size 10 Desending order .
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[10],i;
+    int temp;
+    cout<<"Enter the Numbers";
+    for( i=0;i<10;i++)
+    {
+        cin>>a[i];
+    }
+     
+     
+    
+    for(i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                 temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+
+   for( i=0;i<10;i++)
+    {
+        cout<<" "<<a[i];
+    }
+     
+    
+}

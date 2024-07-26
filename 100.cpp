742bed365be469a644db7a5efd634436b37011a5
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-26 15:15:01 +0530

    write a function to check whether a given string is palindrome or not

---
+++ 100.cpp
@@ -0,0 +1,43 @@
+// write a function to check whether a given string is palindrome or not.
+
+#include<iostream>
+using namespace std;
+
+int pailndrome(char a[100])
+{
+    int i=0,j=0;
+    
+    while(a[i] != '\0')
+    {
+        i++;
+
+    }
+    i--;
+
+    while(i>j)
+    {
+        if(a[j] != a[i])
+        {
+            return -1;
+        }
+        j++;
+        i--;
+    }
+    return 1;
+}
+
+int main()
+{
+     char a[100];
+    cout<<"Enter a string : ";
+    cin.getline(a,100);
+
+    int d=pailndrome(a);
+    if(d==-1)
+    {
+        cout<<"not a palindrome";
+    }
+    else{
+        cout<<" palindrome";
+    }
+}

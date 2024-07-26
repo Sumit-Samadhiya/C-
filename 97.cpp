01b906b01bb71991f03c720dba401a95cf36a9b5
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-26 13:45:39 +0530

    / Write a program to given string into lowercase.

--- 97.cpp
+++ 97.cpp
@@ -1,22 +1,8 @@
-// Write a program to given string into uppercase.
+// Write a program to given string into lowercase.
 
 #include<iostream>
 using namespace std;
 
-
-
-void toUpperCase(char str[]) {
-    int i = 0;
-    while (str[i] != '\0') {
-        
-        if (str[i] >= 'a' && str[i] <= 'z') {
-           
-            str[i] = str[i] - 'a' + 'A';
-        }
-        i++;
-    }
-
-}
 
 void tolowercase(char str[]) {
     int i = 0;
@@ -30,6 +16,7 @@ void tolowercase(char str[]) {
     }
 
 }
+
 int main()
 {
     char a[100];
@@ -37,8 +24,7 @@ int main()
     cout<<"Enter a String : ";
     cin.getline(a,100);
 
-    // toUpperCase(a);
-    // cout<<"UpperCase = "<<a;
+   
     
     tolowercase(a);
     cout<<"UpperCase = "<<a;

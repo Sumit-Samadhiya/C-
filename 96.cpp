cf03d0617ca9e1571a892e99396276f70476b9fb
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-26 21:54:07 +0530

    // Write a program to given string into uppercase.

--- 96.cpp
+++ 96.cpp
@@ -8,7 +8,7 @@ void toUpperCase(char str[]) {
     int i = 0;
     while (str[i] != '\0') {
         
-        if (str[i] >= 'a' && str[i] <= 'z') {
+        if(str[i] >= 'a' && str[i] <= 'z') {
            
             str[i] = str[i] - 'a' + 'A';
         }

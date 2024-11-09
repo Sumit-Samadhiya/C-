167c5debd75c7f0205398b1720bbe209ce4d6b1a
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-09 11:41:23 +0530

    practic

---
+++ practice.py
@@ -0,0 +1,14 @@
+ # 1. Find the largest number among the three numbers.
+
+# a=int(input("Enter a :- "))
+# b=int(input("Enter b :- "))
+# c=int(input("Enter c :- "))
+
+# if(a>b):
+#     if(a>c):
+#         print(a)
+# elif(b>c):
+#     print(b)
+# else:
+#     print(c)
+

84900b9fe349dbcf7d2ebdf1fbcfbadf1756011f
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-18 12:36:29 +0530

    prime

--- practice.py
+++ practice.py
@@ -221,10 +221,13 @@
 #     if is_prime:        
 #         print(num)
 
-def is_prime(n):
-    for i in range (2,n):
-        if n%i==0:
-            return False
-    return True
-        
-print(is_prime(6))
+
+# def is_prime(n):
+#     for i in range (2,n):
+#         if n%i==0:
+#             return False
+#     return True
+# for i in range(1,50):
+#     if is_prime(i):
+#         print(i)
+    

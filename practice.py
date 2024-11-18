427e8c5354b140a1ff5447ad7c907d4ae4429b6c
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-18 12:19:18 +0530

    pr

--- practice.py
+++ practice.py
@@ -15,6 +15,8 @@
 # else:
 #     print(c)
 
+
+
 #2. Write a Program to check whether a number is prime or not.
 
 # def prime(a):
@@ -119,3 +121,93 @@
 
 # reverse('13131')
     
+# Write a Python program to find the factorial of a number. 
+# def factorial(number):
+#     return number * factorial(number-1)
+
+# r=factorial(5)
+# print(r)
+
+
+
+# maximum number
+# def maximum(number):
+#     max= number[0]
+
+#     for i in number:
+#         if i>max:
+#             max=i
+#     return max
+
+# number=[2,44,3,55,2]
+# r=max(number)
+# print(r)
+
+
+
+# sum or Average of a list
+# def avg(n):
+    
+#     total=sum(n)
+#     average= total/len(n)
+
+#     print(average)
+
+# n=[1,2,3,4,5]
+# avg(n)
+
+
+# Find SubString in Text
+
+# def find(string,sub):
+#     if sub in string:
+#         print('yes')
+#     else:
+#         print('no')
+
+# s= "You are Cool"
+# sub= "are"
+# find(s,sub)
+
+
+# Reverse a List
+# def rev(n):
+#     for i in reversed(n):
+#         print(i)
+# n=(1,2,3,4)
+# print(n[::-1])
+# rev(n)
+
+
+#Remove duplicate
+# n=[1,2,3,4,5,5,6,6]
+# unique=set(n)
+# print(unique)
+
+
+
+
+
+# def fibonacci(n):
+
+# # """Calculates the nth Fibonacci number (example function to memoize)."""
+
+#   if n == 0 or n == 1:
+#    return n
+
+#   else:
+
+#    return fibonacci(n-1) + fibonacci(n-2)
+
+# # Example usage
+
+# result = fibonacci(7)  # Memoization avoids redundant calculations
+
+# print(f"Fibonacci of 35: {result}")
+
+
+# n="Sama is cool"
+
+# s=len(n.split(' '))
+# print(s)
+

0353c93e4428900170e7245b9272b25e6eba6740
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-18 12:28:20 +0530

    prime

--- practice.py
+++ practice.py
@@ -212,4 +212,12 @@
 # print(s)
 
 
-
+for num in range(100,200):
+    is_prime=True
+    for i in range(2,num):
+        if num%i==0:
+            is_prime=False
+            break
+    if is_prime:        
+        print(num)
+        

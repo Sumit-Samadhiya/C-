6a82b3a9b11f1de267f43bb211b20450e623a628
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-18 12:34:25 +0530

    prime

--- practice.py
+++ practice.py
@@ -212,12 +212,19 @@
 # print(s)
 
 
-for num in range(100,200):
-    is_prime=True
-    for i in range(2,num):
-        if num%i==0:
-            is_prime=False
-            break
-    if is_prime:        
-        print(num)
+# for num in range(1,200):
+#     is_prime=True
+#     for i in range(2,num):
+#         if num%i==0:
+#             is_prime=False
+#             break
+#     if is_prime:        
+#         print(num)
+
+def is_prime(n):
+    for i in range (2,n):
+        if n%i==0:
+            return False
+    return True
         
+print(is_prime(6))

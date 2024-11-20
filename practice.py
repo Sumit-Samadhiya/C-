043215c8b71758a4de8cdf5a035f8621d690c939
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-20 16:40:00 +0530

    max

--- practice.py
+++ practice.py
@@ -390,14 +390,14 @@
 # r= max(n)
 # print(r)
 
-def find_largest(numbers):
-    largest = numbers[0]
-    for num in numbers:
-        if num > largest:
-            largest = num
-    return largest
+# def find_largest(numbers):
+#     largest = numbers[0]
+#     for num in numbers:
+#         if num > largest:
+#             largest = num
+#     return largest
 
-# Test the function
-nums = [10, 5, 8, 20, 3]
-largest_num = find_largest(nums)
-print(f"The largest number is {largest_num}")
+# # Test the function
+# nums = [10, 5, 8, 20, 3]
+# largest_num = find_largest(nums)
+# print(f"The largest number is {largest_num}")

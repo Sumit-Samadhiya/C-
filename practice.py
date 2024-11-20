7099e6c018e7010acc08f2a0e8228b5674939c2f
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-20 16:39:10 +0530

    max

--- practice.py
+++ practice.py
@@ -389,3 +389,15 @@
 # n= [1,2,3,8,5,6]
 # r= max(n)
 # print(r)
+
+def find_largest(numbers):
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
+
+# Test the function
+nums = [10, 5, 8, 20, 3]
+largest_num = find_largest(nums)
+print(f"The largest number is {largest_num}")

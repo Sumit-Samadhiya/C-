a139dd2b13c855ada405313652ca3134ac42546f
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-24 11:57:45 +0530

    fib

--- practice.py
+++ practice.py
@@ -474,14 +474,15 @@
 #     print(f)
 # fibo(8)
 
-# def fib(n):
-#     if n<= 0:
-#         return
-#     f=[0,1]
-#     for i in range(2,n):
-#         f.append(f[i-1]+f[i-2])
-#     print(f)
-# fib(9)
+def fib(n):
+   
+    f=[0,1]
+    for i in range(2,n):
+        f.append(f[i-1]+f[i-2])
+    print(f)
+fib(9)
+
+
 
 
 

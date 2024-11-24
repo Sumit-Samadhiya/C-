05e27e01fb8aa9e048fe222a539d86f68f3c366b
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-11-24 12:55:31 +0530

    async

--- practice.js
+++ practice.js
@@ -14,17 +14,17 @@
 //   });
 
 
-  async function fetchData() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json(); // Parse JSON response
-      console.log(data); // Print the data to the console
-    } catch (error) {
-      console.error('Error:', error); // Handle and log any errors
-    }
-  }
+//   async function fetchData() {
+//     try {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch data');
+//       }
+//       const data = await response.json(); // Parse JSON response
+//       console.log(data); // Print the data to the console
+//     } catch (error) {
+//       console.error('Error:', error); // Handle and log any errors
+//     }
+//   }
   
-  fetchData();
+//   fetchData();

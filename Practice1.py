873415d86e0acd6837d8947aee132e1fff7eab52
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-01-23 16:57:13 +0530

    ..

--- Practice1.py
+++ Practice1.py
@@ -18,5 +18,10 @@
 # a= int(input('Enter no:- '))
 # print(a%10)
 
-
+# 4. Leap Year or Not
+a= int(input('Enter :-  '))
+if(a%4==0):
+    if(a%100!=0 or a%400==0):
+     print('leap year')
+       
 

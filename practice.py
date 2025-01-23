9aee3d9ee48be5a2172392d7eba909cf4fb3f328
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-01-23 15:08:51 +0530

    ..

--- practice.py
+++ practice.py
@@ -480,12 +480,12 @@
 #     print(f)
 # fib(9)
 
-def fibo(n):
-    if n<=2:
-        return n-1
-    else:
-        return fibo(n-1)+fibo(n-2)
-print(fibo(4))
+# def fibo(n):
+#     if n<=2:
+#         return n-1
+#     else:
+#         return fibo(n-1)+fibo(n-2)
+# print(fibo(4))
 
 
 

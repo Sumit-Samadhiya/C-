8bc9637554ac307d5c0b67fe2dcaf1adc2553d2e
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-02-01 08:37:54 +0530

    ..

--- 1.py
+++ 1.py
@@ -1,7 +1,12 @@
 # Write a program to print "Hello, World!".
 
-print('Helllo World')
-
+# print('Helllo World')
+# ----------------------------------------------------------------------------------------------------------
 
 # Write a function to add two numbers.
 
+def fun(a,b):
+    c=a+b
+    print(c)
+
+fun(a=3,b=5)

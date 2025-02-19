c83e4ad41f89ebf97ce86072c3c961b054b375b7
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-02-19 12:13:34 +0530

    ..

---
+++ Practice1.py
@@ -0,0 +1,16 @@
+# i= int(input('Enter :- '))
+# if(i%2==0):
+#     print('even')
+# else:
+#     print('odd')
+
+# i= int(input('Enter :- '))
+# unit=i%10
+# print(unit)
+
+# i=int(input('enter :-'))
+# if(i>=100 and i<=999):
+#     print("tree digit no")
+# else:
+#     print('Not a three digit no.')
+

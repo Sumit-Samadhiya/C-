273a511a1f7231d6fb6074942bfc2645110e1680
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-04-07 22:22:17 +0530

    ...

--- practice.cpp
+++ practice.cpp
@@ -4,8 +4,14 @@ using namespace std;
 int main()
 {
     // printf("Ram Ram");
-    
+
     int a,b;
-    cin>>a>>b;
-    cout<<a+b;
+    // cin>>a>>b;
+    // cout<<a+b;/
+
+    cin>>a;
+    cout<<a*a;
+
+    
+
 }
